bad12e0e0c3c51f51414e65d27c53c70bfdf1457
1523550440@qq.com <1523550440@qq.com>
2020-07-12 15:44:35 +0800

    --提交代码

--- spring-mvc/src/main/java/com/xbang/mvc/SpringMvcApplication.java
+++ spring-mvc/src/main/java/com/xbang/mvc/SpringMvcApplication.java
@@ -1,7 +1,6 @@
 package com.xbang.mvc;
 
 import com.xbang.commons.entity.LocalDateTimeEntity;
-import com.xbang.multi.datasource.datasource.advisor.DynamicDataSourceAnnotationAdvisor;
 import com.xbang.mvc.annotation.UserId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -17,14 +16,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = {"com.xbang.mvc","com.xbang.mybatis"})
+@SpringBootApplication(scanBasePackages = {"com.xbang.mvc"})
 @RestController
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
         ApplicationContext context  = SpringApplication.run(SpringMvcApplication.class, args);
-        DynamicDataSourceAnnotationAdvisor dynamicDataSourceAnnotationAdvisor = context.getBean(DynamicDataSourceAnnotationAdvisor.class);
-        Assert.notNull(dynamicDataSourceAnnotationAdvisor,"找不到这个bean");
     }
 
     @GetMapping("/test")
